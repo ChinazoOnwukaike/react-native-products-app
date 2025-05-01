b9cb253f2b04e0d8037698565b87d5bb6ebfdf9c
Chinazo <chinazoonwukaike@gmail.com>
2025-05-01 13:13:46 -0500

    add project fonts, and default colors

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,10 +1,18 @@
 import { View } from "react-native";
 import React from "react";
 import { ThemedText } from "@/presentation/auth/theme/components/ThemedText";
+import { useThemeColor } from "@/presentation/auth/theme/hooks/useThemeColor";
 
 const Home = () => {
+  const primary = useThemeColor({}, "primary");
+
   return (
-    <View>
+    <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
+      <ThemedText style={{ fontFamily: "KanitBold", color: primary }}>
+        Home
+      </ThemedText>
+      <ThemedText style={{ fontFamily: "KanitRegular" }}>Home</ThemedText>
+      <ThemedText style={{ fontFamily: "KanitThin" }}>Home</ThemedText>
       <ThemedText>Home</ThemedText>
     </View>
   );
